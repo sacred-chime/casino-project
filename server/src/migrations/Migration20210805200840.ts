c26b8799839e7f97c830393011ee6612a47b27ea
ReerandRee <lelandsly@outlook.com>
2021-08-05 16:24:46 -0400

    setup
    setup

--- server/src/migrations/Migration20210805200840.ts
+++ server/src/migrations/Migration20210805200840.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20210727000023 extends Migration {
+export class Migration20210805200840 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table "user" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "username" text not null, "email" text not null, "password" text not null);');
