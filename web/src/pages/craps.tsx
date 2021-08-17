63c3bf358440e939a6ee0e714ffce36730194e17
Harrison Nguyen <hxnguyen3@gmail.com>
2021-08-17 16:35:30 -0400

    Added more logic to the game of craps
    Craps header was added alongside the Roll Dice button. Almost all of the logic for the game of craps was added. The logic for rolling a non-winning and non-losing number needs to be added next.

--- web/src/pages/craps.tsx
+++ web/src/pages/craps.tsx
@@ -1,67 +1,205 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, Button, Center, SimpleGrid } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react"
 import { withUrqlClient } from "next-urql";
-import React from "react";
+import React,{useState} from "react";
 import { InterfaceUI } from "../components/InterfaceUI";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { useIsAuth } from "../utils/useIsAuth";
 import { getRandomInt } from "../utils/getRandomInt";
+import { Image } from "@chakra-ui/react"
+import { Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react"
+import { CloseButton } from "@chakra-ui/react"
+import diceFace1 from "../assets/dice-six-faces-one.png";
+import diceFace2 from "../assets/dice-six-faces-two.png";
+import diceFace3 from "../assets/dice-six-faces-three.png";
+import diceFace4 from "../assets/dice-six-faces-four.png";
+import diceFace5 from "../assets/dice-six-faces-five.png";
+import diceFace6 from "../assets/dice-six-faces-six.png";
 
+const DICE_FACE: any = {
+    '⚀': diceFace1,
+    '⚁': diceFace2,
+    '⚂': diceFace3,
+    '⚃': diceFace4,
+    '⚄': diceFace5,
+    '⚅': diceFace6
+}
+
+// This function returns a random die
 const getRandomDie = (): any => {
     const diceSides = ['⚀','⚁','⚂', '⚃', '⚄','⚅'];
   
     return diceSides[getRandomInt(0, diceSides.length)];
-  };
-
-const Tile: React.FC<any> = (props: any) => {
-    return (
-      <Box bg="rgb(88,88,88)" height="100px" textAlign="center" paddingTop="38px">
-        {props.value}
-      </Box>
-    );
-  };
-
-const Board: React.FC<{}> = ({}) => {
-    let boardTiles = [];
-    for (let i = 0; i < 2; i++) {
-      const die: any = getRandomDie();
-      boardTiles.push(
-        <Tile key={`[0, ${i}]`} value={die}/>
-      );
-    }
-    return (
-      <Box
-        margin={"auto"}
-        width={"25%"}
-        marginTop={"100px"}
-        padding={"10px"}
-        bg="rgb(88,88,88)"
-      >
-        <SimpleGrid columns={2} rows={1} spacing={2}>
-          {boardTiles}
-        </SimpleGrid>
-      </Box>
-    );
-  };
-
-
-
-
-const Craps: React.FC<{}> = ({}) => {
-  useIsAuth();
-  return (
-    <>
-      <InterfaceUI>
-        <div>Craps</div>
-        <Board></Board>
-      </InterfaceUI>
-    </>
-  );
 };
 
-export default withUrqlClient(createUrqlClient)(Craps);
+// This function converts unicode dice to its image
+const convertUnicode = (unicode: any): any => {
+    const diceSides = ['⚀','⚁','⚂', '⚃', '⚄','⚅'];
+  
+    return DICE_FACE[unicode];
+};
 
+// function convertUnicode(unicode: any) {
+//     return DICE_FACE[unicode];
+// }
 
+// This function calculates the sum of two dice sides
+const diceSum = (dice1: any, dice2: any): any => {
+    return (dice1 + dice2);
+};
 
+// function diceSum(dice1: any, dice2: any) {
+//     return (dice1 + dice2);
+// }
+
+// This function converts unicode to its integer 
+const getDiceInt = (diceUni: any): any => {
+    let diceInt = 0;
+
+    if (diceUni === '⚀') {
+        diceInt = 1;
+    } else if (diceUni === '⚁') {
+        diceInt = 2;
+    } else if (diceUni === '⚂') {
+        diceInt = 3;
+    } else if (diceUni === '⚃') {
+        diceInt = 4;
+    } else if (diceUni === '⚄') {
+        diceInt = 5;
+    } else if (diceUni === '⚅'){
+        diceInt = 6;
+    } else {
+        console.log('Wrong input');
+    }
+
+    return diceInt;
+};
+
+// function getDiceInt(diceUni: any) {
+//     let diceInt = 0;
+
+//     if (diceUni === '⚀') {
+//         diceInt = 1;
+//     } else if (diceUni === '⚁') {
+//         diceInt = 2;
+//     } else if (diceUni === '⚂') {
+//         diceInt = 3;
+//     } else if (diceUni === '⚃') {
+//         diceInt = 4;
+//     } else if (diceUni === '⚄') {
+//         diceInt = 5;
+//     } else if (diceUni === '⚅'){
+//         diceInt = 6;
+//     } else {
+//         console.log('Wrong input');
+//     }
+
+//     return diceInt;
+// }
+
+const Tile: React.FC<any> = (props: any) => {
+
+    return (
+      <Box bg="rgb(88,88,88)" height="100px" textAlign="center" paddingTop="38px">
+          {props.value}
+          {<Image src={convertUnicode(props.value)}/>}
+      </Box>
+    );
+};
+
+const Win: React.FC<any> = (props: any) => {
+    console.log("Lose React method");
+    return (
+        <Alert status="success">
+        <AlertIcon />
+        <Box flex="1">
+        <AlertTitle>Success!</AlertTitle>
+        <AlertDescription display="block">
+        Your application has been received. We will review your application and
+        respond within the next 48 hours.
+        </AlertDescription>
+        </Box>
+        <CloseButton position="absolute" right="8px" top="8px" />
+        </Alert>
+    );
+};
+
+const Lose: React.FC<any> = (props: any) => {
+    console.log("Lose React method");
+    return (
+        <Alert status="success">
+        <AlertIcon />
+        <Box flex="1">
+        <AlertTitle>Success!</AlertTitle>
+        <AlertDescription display="block">
+        Your application has been received. We will review your application and
+        respond within the next 48 hours.
+        </AlertDescription>
+        </Box>
+        <CloseButton position="absolute" right="8px" top="8px" />
+        </Alert>
+    );
+};
+
+const Board: React.FC<{}> = ({}) => {
+
+    const [diceDisplay, setDiceDisplay] = useState<any>(['START', 'GAME'])
+    
+    const rollDice = () => {
+        let diceSide = [];
+        for (let i = 0; i < 2; i++) {
+            const die: any = getRandomDie();
+            diceSide.push(
+              die
+            );
+          }
+        setDiceDisplay(diceSide);
+        let diceValue1 = getDiceInt(diceSide[0]);
+        let diceValue2 = getDiceInt(diceSide[1]);
+        let sumOfDice = diceSum(diceValue1,diceValue2);
+
+        console.log("The sum is"+sumOfDice);
+
+        if (sumOfDice === 7 || sumOfDice === 11) {
+            console.log('YOU WON!!!!!!');
+            <Win></Win>
+        } else if (sumOfDice === 2 || sumOfDice === 3 || sumOfDice === 12) {
+            console.log('YOU LOST!!!!!!');
+            <Lose></Lose>
+        } else {
+            console.log('Other');
+        }
+
+        let marker = sumOfDice;
+
+        console.log("This is the marker: "+marker);
+
+        // while(true) {
+        //     // let markerDiceSide = [];
+        //     // for (let i = 0; i < 2; i++) {
+        //     //     const die: any = getRandomDie();
+        //     //     markerDiceSide.push(
+        //     //         die
+        //     //     );
+        //     // }
+        //     // setDiceDisplay(markerDiceSide);
+        //     let markerDiceValue1 = getDiceInt(diceSide[0]);
+        //     let markerDiceValue2 = getDiceInt(diceSide[1]);
+        //     let markerSumOfDice = diceSum(markerDiceValue1,markerDiceValue2);
+
+        //     console.log('Inside marker.... '+ markerSumOfDice);
+
+        //     if (sumOfDice === marker) {
+        //         console.log('YOU WON...marker!!!!!!');
+        //         <Win></Win>
+        //         break;
+        //     } else if (sumOfDice === 7) {
+        //         console.log('YOU LOST...marker!!!!!!');
+        //         <Lose></Lose>
+        //         break;
+        //     }
+        // }
+    }
 /*
 If his first throw is 7 or 11 and we bet the pass line we all win
 If first roll is 2, 3, 12 we all loss
@@ -69,3 +207,39 @@ If first roll is 2, 3, 12 we all loss
 marker is now on number. Keeps rolling dice until he rolls that number. 
 Roll number before 7, we all win
 */
+    
+    return (
+    <>
+    <Center><Heading marginTop={"50px"} color='red'>Craps ⚅</Heading></Center>
+    <Box
+        margin={"auto"}
+        width={"25%"}
+        marginTop={"50px"}
+        padding={"10px"}
+        bg="rgb(88,88,88)"
+    >
+    <SimpleGrid columns={2} rows={1} spacing={2}>
+        {diceDisplay.map((die: any, i: any) => (<Tile key={`[0, ${i}]`} value={die}/>))}
+    </SimpleGrid>
+    </Box>
+    <Center marginTop={"40px"}>
+    <Button colorScheme="red" onClick={rollDice}>
+        Roll Dice
+    </Button>
+    </Center>
+    </>
+    );
+  };
+
+const Craps: React.FC<{}> = ({}) => {
+  useIsAuth();
+  return (
+    <>
+      <InterfaceUI>
+        <Board></Board>
+      </InterfaceUI>
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(Craps);
