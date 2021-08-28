ac6e380c17896ebd12fc426cebeee7a720b18337
Connor <cmstroh99@gmail.com>
2021-08-28 17:32:05 -0400

    minesweeper fixes
    minesweeper fixes

--- web/src/pages/minesweeper.tsx
+++ web/src/pages/minesweeper.tsx
@@ -1,316 +1,307 @@
 import { Box, Button, Center, SimpleGrid } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { InterfaceUI } from "../components/InterfaceUI";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { getRandomInt } from "../utils/getRandomInt";
-import { useIsAuth } from "../utils/useIsAuth";
+import { isValidCoord } from "../utils/minesweeper/isValidCoord";
 import { sumAdjacents } from "../utils/minesweeper/sumAdjacents";
+import { useIsAuth } from "../utils/useIsAuth";
 
 // CONSTANTS
-const n = 7; // size of (n x n) matrix
+const n = 20; // size of (n x n) matrix
 
 // TYPESCRIPT DEFINITIONS
 interface MinesweeperTilesProps {
-    matrix: Tile[][];
-    updateTile: (row: number, column: number) => void;
-    endGameState: () => void;
-    //updateGameState: () => void;
-    //isGameStarted?: boolean;
-    //setIsGameStarted?: React.Dispatch<React.SetStateAction<boolean>>;
+  matrix: MinesweeperTile[][];
+  updateTile: (row: number, column: number) => void;
+  setAllTilesVisibility: (visible: boolean) => void;
 }
 
-interface Tile {
-    hasBomb: boolean;
-    adjacentCount: number;
-    isVisible: boolean;
+export interface MinesweeperTile {
+  hasBomb: boolean;
+  adjacentCount: number;
+  isVisible: boolean;
 }
 
 interface MinesweeperUIProps {
-    updateGameState: () => void;
-    // setSeconds: function useEffect(effect: React.EffectCallback, deps?: React.DependencyList | undefined): void
-    startTimer: () => void;
-    matrix: Tile[][];
-    seconds: number;
-    setSeconds: React.Dispatch<React.SetStateAction<number>>;
-    isGameStarted: boolean;
-    setIsGameStarted: React.Dispatch<React.SetStateAction<boolean>>;
-    //decrementTest: () => void;
-    endGameState: () => void;
+  seconds: number;
+  startTimer: () => void;
+  isGameStarted: boolean;
+  updateGameState: () => void;
 }
 
 // MAIN COMPONENT
 const Minesweeper: React.FC<{}> = ({}) => {
-    useIsAuth();
-    const hold: Tile = { hasBomb: false, adjacentCount: 0, isVisible: true };
+  useIsAuth();
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [seconds, setSeconds] = useState(90);
+  // Default TileState
+  const defaultTileState: MinesweeperTile = {
+    hasBomb: false,
+    adjacentCount: 0,
+    isVisible: true,
+  };
+  const defaultTileMatrix = Array.from({ length: n }, () =>
+    Array.from({ length: n }, () => defaultTileState)
+  );
+  const [matrix, setMatrix] = useState<MinesweeperTile[][]>(defaultTileMatrix);
 
-    const [matrix, setMatrix] = useState<Tile[][]>(
-        //tilePlacement()
-        // initial numbers filled here
-        // first array for number of rows
-        Array.from({ length: n }, () => Array.from({ length: n }, () => hold))
-    ); // use string blanks for starting off
-    const [isGameStarted, setIsGameStarted] = useState(false);
+  const startTimer = () => {
+    console.log("Game started.");
+    useEffect(() => {
+      if (seconds > 0) {
+        setTimeout(() => setSeconds(seconds - 1), 1000);
+        // console.log("countdown works");
+      } else {
+        // console.log("end of countdown");
+        setSeconds(0);
+      }
+    });
+  };
 
-    const [seconds, setSeconds] = useState(90);
-
-    /*
-    const decrementTest = () => {
-        setSeconds(seconds - 1);
-    };
-    */
-
-    const startTimer = () => {
-        console.log("function is called");
-        //if (isGameStarted === true) {
-        console.log("Game started, no countdown tho");
-        useEffect(() => {
-            if (seconds > 0) {
-                setTimeout(() => setSeconds(seconds - 1), 1000);
-                console.log("countdown works");
-            } else {
-                console.log("end of countdown");
-                setSeconds(0);
-            }
-        });
-        //}
-    };
-    // game does not reset board - adds more bombs instead
-    const updateGameState = () => {
-        if (isGameStarted === false) {
-            setIsGameStarted(true);
-            let newMatrix = bombSet(matrix);
-            newMatrix = bombCount(newMatrix);
-            for (let i = 0; i < newMatrix.length; i++) {
-                for (let j = 0; j < newMatrix[i].length; j++) {
-                    newMatrix[i][j] = {
-                        hasBomb: newMatrix[i][j].hasBomb,
-                        adjacentCount: newMatrix[i][j].adjacentCount,
-                        isVisible: false,
-                    };
-                }
-            }
-            setMatrix(newMatrix);
-        } else {
-            setIsGameStarted(false);
+  // START / RESET BUTTON
+  const updateGameState = () => {
+    if (isGameStarted === false) {
+      // START GAME
+      setIsGameStarted(true);
+      // POPULATE WITH BOMBS
+      let newMatrix = setBombLocations(defaultTileMatrix);
+      newMatrix = setBombAdjacencyValues(newMatrix);
+      for (let i = 0; i < newMatrix.length; i++) {
+        for (let j = 0; j < newMatrix[i].length; j++) {
+          newMatrix[i][j] = {
+            hasBomb: newMatrix[i][j].hasBomb,
+            adjacentCount: newMatrix[i][j].adjacentCount,
+            isVisible: false,
+          };
         }
-        // console.log("GAME START");
-    };
+      }
+      setMatrix(newMatrix);
+    } else {
+      // RESET GAME
+      setIsGameStarted(false);
+      setMatrix(defaultTileMatrix);
+      setAllTilesVisibility(false);
+    }
+  };
 
-    // explicit game reset - yet to try/test
-    const resetGameBoard = () => {
-        if (isGameStarted === false) {
-            setMatrix(
-                Array.from({ length: n }, () =>
-                    Array.from({ length: n }, () => hold)
-                )
-            );
-        }
-    };
+  // MINESWEEPER TILE onClick
+  const updateTile = (row: number, column: number) => {
+    let copy = createUpdateTileMatrix([...matrix], row, column);
+    console.log(copy);
+    if (checkForEndGame(copy)) {
+      // IF GAME IS WON
+      alert("YOU WON!");
+      console.log("YOU WON!");
+      setAllTilesVisibility(true);
+    } else {
+      setMatrix(copy);
+    }
+  };
 
-    const endGameState = () => {
-        if (isGameStarted === true) {
-            for (let i = 0; i < matrix.length; i++) {
-                for (let j = 0; j < matrix[i].length; j++) {
-                    matrix[i][j] = {
-                        hasBomb: matrix[i][j].hasBomb,
-                        adjacentCount: matrix[i][j].adjacentCount,
-                        isVisible: true,
-                    };
-                }
-            }
-            setIsGameStarted(false);
-            //alert("GAME OVER");
-        }
-    };
+  // SETS ALL TILES TO visible INPUT BOOLEAN VALUE
+  const setAllTilesVisibility = (visible: boolean) => {
+    let copy = [...matrix];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        copy[i][j] = {
+          hasBomb: matrix[i][j].hasBomb,
+          adjacentCount: matrix[i][j].adjacentCount,
+          isVisible: visible,
+        };
+      }
+      setMatrix(copy);
+    }
+  };
 
-    const updateTile = (row: number, column: number) => {
-        let copy = [...matrix];
-
-        // try to implement properties for matrix tiles
-        if (copy[row][column].isVisible === false) {
-            copy[row][column] = {
-                hasBomb: copy[row][column].hasBomb,
-                adjacentCount: copy[row][column].adjacentCount,
-                isVisible: true,
-            };
-        }
-
-        setMatrix(copy);
-
-        console.log(copy);
-    };
-
-    const checkEndGame = (matrix: Tile[][]) => {
-        console.log("checkEndGame is called");
-        for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix[i].length; j++) {
-                if (
-                    matrix[i][j].hasBomb === false &&
-                    matrix[i][j].isVisible === false
-                ) {
-                    break;
-                }
-            }
-        }
-        endGameState();
-    };
-
-    return (
-        <>
-            <InterfaceUI>
-                <Box>
-                    <MinesweeperUI
-                        //decrementTest={decrementTest}
-                        updateGameState={updateGameState}
-                        startTimer={startTimer}
-                        seconds={seconds}
-                        isGameStarted={isGameStarted}
-                        setSeconds={setSeconds}
-                        endGameState={endGameState}
-                        setIsGameStarted={setIsGameStarted}
-                        matrix={matrix}
-                    />
-                    <MinesweeperTiles
-                        matrix={matrix}
-                        updateTile={updateTile}
-                        endGameState={endGameState}
-                    />
-                </Box>
-            </InterfaceUI>
-        </>
-    );
+  return (
+    <>
+      <InterfaceUI>
+        <Box>
+          <MinesweeperUI
+            seconds={seconds}
+            startTimer={startTimer}
+            isGameStarted={isGameStarted}
+            updateGameState={updateGameState}
+          />
+          <MinesweeperTiles
+            matrix={matrix}
+            updateTile={updateTile}
+            setAllTilesVisibility={setAllTilesVisibility}
+          />
+        </Box>
+      </InterfaceUI>
+    </>
+  );
 };
 
 export default withUrqlClient(createUrqlClient)(Minesweeper);
 
 // MINESWEEPER UI
-
 const MinesweeperUI: React.FC<MinesweeperUIProps> = ({
-    updateGameState,
-    endGameState,
-    setSeconds,
-    isGameStarted,
-    seconds,
-    startTimer,
-    //decrementTest,
-    setIsGameStarted,
-    matrix,
+  seconds,
+  startTimer,
+  isGameStarted,
+  updateGameState,
 }) => {
-    return (
-        <Box>
-            <Button
-                onClick={() => {
-                    updateGameState();
-                }}
-            >
-                Start
-            </Button>
-            <Box>{seconds}</Box>
-        </Box>
-    );
+  return (
+    <Box>
+      <Button
+        onClick={() => {
+          updateGameState();
+        }}
+      >
+        {isGameStarted ? "RESET" : "START"}
+      </Button>
+      <Box>{seconds}</Box>
+    </Box>
+  );
 };
 
 // MINESWEEPER TILE GRID
 const MinesweeperTiles: React.FC<MinesweeperTilesProps> = ({
-    matrix,
-    updateTile,
-    endGameState,
-    //updateGameState,
-    //isGameStarted,
-    //setIsGameStarted,
+  matrix,
+  updateTile,
+  setAllTilesVisibility,
 }) => {
-    return (
-        <Box bgColor={"blue.700"}>
-            {matrix.map((row, rowIndex) => (
-                <SimpleGrid key={rowIndex} columns={n}>
-                    {row.map((column, columnIndex) => (
-                        <Center py={2} key={columnIndex}>
-                            <Button
-                                bgColor={"orange.200"}
-                                width={"60%"}
-                                isDisabled={
-                                    matrix[rowIndex][columnIndex].isVisible
-                                }
-                                onClick={() => {
-                                    updateTile(rowIndex, columnIndex);
-                                    if (matrix[rowIndex][columnIndex].hasBomb) {
-                                        endGameState();
-                                    }
-                                }}
-                            >
-                                {matrix[rowIndex][columnIndex].isVisible
-                                    ? matrix[rowIndex][columnIndex].hasBomb
-                                        ? "💣"
-                                        : "" +
-                                          matrix[rowIndex][columnIndex]
-                                              .adjacentCount
-                                    : " "}
-                            </Button>
-                        </Center>
-                    ))}
-                </SimpleGrid>
-            ))}
-        </Box>
-    );
+  return (
+    <Box bgColor={"blue.700"}>
+      {matrix.map((row, rowIndex) => (
+        <SimpleGrid key={rowIndex} columns={n}>
+          {row.map((column, columnIndex) => (
+            <Center py={2} key={columnIndex}>
+              <Button
+                bgColor={"orange.200"}
+                width={"60%"}
+                isDisabled={matrix[rowIndex][columnIndex].isVisible}
+                onClick={() => {
+                  updateTile(rowIndex, columnIndex);
+                  if (matrix[rowIndex][columnIndex].hasBomb) {
+                    setAllTilesVisibility(true);
+                  }
+                }}
+              >
+                {matrix[rowIndex][columnIndex].isVisible
+                  ? matrix[rowIndex][columnIndex].hasBomb
+                    ? "💣"
+                    : "" + matrix[rowIndex][columnIndex].adjacentCount
+                  : " "}
+              </Button>
+            </Center>
+          ))}
+        </SimpleGrid>
+      ))}
+    </Box>
+  );
 };
 
 // HELPER / PLACEHOLDER FUNCTIONS
 
-// Add properties to tiles
-const tilePlacement = () => {
-    let tiles: Tile[][] = [];
-
-    for (let i = 0; i < n; i++) {
-        tiles.push([]);
-        for (let j = 0; j < n; j++) {
-            tiles[i].push({
-                hasBomb: false,
-                adjacentCount: 0,
-                isVisible: false,
-            });
-        }
-    }
-
-    bombSet(tiles);
-    bombCount(tiles);
-    console.log("testing the things:");
-    console.log(tiles);
-    return tiles;
-};
-
 // Bomb Placement
-const bombSet = (tiles: Tile[][]) => {
-    let bombsPlaced = 0;
-
-    while (bombsPlaced < 7) {
-        const rowRandom = Math.floor(getRandomInt(0, tiles.length));
-        const colRandom = Math.floor(getRandomInt(0, tiles[0].length));
-
-        tiles[rowRandom][colRandom] = {
-            hasBomb: true,
-            adjacentCount: tiles[rowRandom][colRandom].adjacentCount,
-            isVisible: tiles[rowRandom][colRandom].isVisible,
-        };
-
-        // cells[rowRandom][colRandom].textContent = 'H'
-        bombsPlaced++;
-    }
-    return tiles;
+const setBombLocations = (tiles: MinesweeperTile[][]) => {
+  let bombsPlaced = 0;
+  const maxNumberOfBombs = Math.round(
+    (tiles.length * tiles[0].length) / 6.4 + 1
+  );
+  while (bombsPlaced < maxNumberOfBombs) {
+    const rowRandom = Math.floor(getRandomInt(0, tiles.length));
+    const colRandom = Math.floor(getRandomInt(0, tiles[0].length));
+    tiles[rowRandom][colRandom] = {
+      hasBomb: true,
+      adjacentCount: tiles[rowRandom][colRandom].adjacentCount,
+      isVisible: tiles[rowRandom][colRandom].isVisible,
+    };
+    bombsPlaced++;
+  }
+  return tiles;
 };
 
 // Sum Adjacent
-const bombCount = (tiles: Tile[][]) => {
-    for (let i = 0; i < tiles.length; i++) {
-        for (let j = 0; j < tiles[0].length; j++) {
-            const newAdjacent = sumAdjacents(tiles, i, j);
-            tiles[i][j] = {
-                hasBomb: tiles[i][j].hasBomb,
-                adjacentCount: newAdjacent,
-                isVisible: tiles[i][j].isVisible,
-            };
-        }
+const setBombAdjacencyValues = (tiles: MinesweeperTile[][]) => {
+  for (let i = 0; i < tiles.length; i++) {
+    for (let j = 0; j < tiles[0].length; j++) {
+      const newAdjacent = sumAdjacents(tiles, i, j);
+      tiles[i][j] = {
+        hasBomb: tiles[i][j].hasBomb,
+        adjacentCount: newAdjacent,
+        isVisible: tiles[i][j].isVisible,
+      };
     }
+  }
 
-    return tiles;
+  return tiles;
+};
+
+// Check if the game has been won
+const checkForEndGame = (matrix: MinesweeperTile[][]) => {
+  let gameWon = false;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j].hasBomb === false && matrix[i][j].isVisible === false) {
+        return gameWon;
+      }
+    }
+  }
+  gameWon = true;
+  return gameWon;
+};
+
+// CREATE NEW UPDATETILE MATRIX
+const createUpdateTileMatrix = (
+  matrix: MinesweeperTile[][],
+  row: number,
+  column: number
+) => {
+  if (matrix[row][column].isVisible === false) {
+    matrix[row][column] = {
+      hasBomb: matrix[row][column].hasBomb,
+      adjacentCount: matrix[row][column].adjacentCount,
+      isVisible: true,
+    };
+  }
+
+  if (
+    matrix[row][column].adjacentCount === 0 &&
+    matrix[row][column].hasBomb === false
+  ) {
+    // all adjacent moves
+    let neighborMoves = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ];
+    // creating an array of all valid adjacent coordinates
+    const neighbors = [];
+    for (let i = 0; i < neighborMoves.length; i++) {
+      const curr_neighbor = neighborMoves[i];
+      const curr_coordinate = [
+        curr_neighbor[0] + row,
+        curr_neighbor[1] + column,
+      ];
+      if (isValidCoord(curr_coordinate, matrix)) {
+        neighbors.push(curr_coordinate);
+      }
+    }
+    // setting adjacent
+    for (let j = 0; j < neighbors.length; j++) {
+      if (
+        matrix[neighbors[j][0]][neighbors[j][1]].isVisible === false &&
+        matrix[neighbors[j][0]][neighbors[j][1]].adjacentCount === 0 &&
+        matrix[neighbors[j][0]][neighbors[j][1]].hasBomb === false
+      ) {
+        matrix = createUpdateTileMatrix(
+          matrix,
+          neighbors[j][0],
+          neighbors[j][1]
+        );
+      }
+    }
+  }
+  return matrix;
 };
